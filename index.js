eafb49c7c246999385ce54de10a1d6246be969cd
rthelolchex <rthelolchex@gmail.com>
2021-09-13 23:47:47 -0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const hoyolab = require('./lib/hoyolab')
 const daily = require('./lib/daily')
 const transactions = require('./lib/transactions')
+const gacha = require('./lib/gacha')
 const fs = require('fs')
 const { UID, COOKIE_TOKEN, SERVER, LANGUAGE, LANGUAGE_REASON, AUTH_TOKEN } = require('./config.json')
 
 
 async function main() {
-    let code = '10241278934'
-    let reason = await transactions.recognizeReason(code)
-    console.log(reason)
+    let history = await gacha.getWishHistory('1234')
+    console.log(history)
 }
 
 main()
