5bed38491853d98b87b732da5f7cad86a943f67a
rthelolchex <rthelolchex@gmail.com>
2021-09-13 23:29:37 -0700

    another update

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const hoyolab = require('./lib/hoyolab')
 const daily = require('./lib/daily')
-const { UID, COOKIE_TOKEN, SERVER } = require('./config.json')
+const transactions = require('./lib/transactions')
+const fs = require('fs')
+const { UID, COOKIE_TOKEN, SERVER, LANGUAGE, LANGUAGE_REASON, AUTH_TOKEN } = require('./config.json')
+
 
 async function main() {
-    const reedemcode = await hoyolab.ClaimReedemCode(COOKIE_TOKEN, UID, SERVER, 'GENSHINGIFT')
-    if (reedemcode) {
-        let str = `
-== REEDEM CODE ==
-
-Result : ${reedemcode.message}
-
-== Powered by ExBot - Project by rthelolchex ==
-        `.trim()
-        console.log(str)
-    }
+    let code = '10241278934'
+    let reason = await transactions.recognizeReason(code)
+    console.log(reason)
 }
 
 main()
