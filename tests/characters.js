5e3a20d0577dde731cac0bdb97bc0edb9d3f4691
rthelolchex <rthelolchex@gmail.com>
2021-09-28 12:37:56 +0700

    fixing the system is busy for ClaimReedemCode function

--- tests/characters.js
+++ tests/characters.js
@@ -2,8 +2,11 @@ const gi = require('../lib/functions')
 const config = require('../config.json')
 async function getCharacters(cookie, uid) {
     const data = await gi.GetUserProfile(cookie, uid)
-    const result = await gi.simpleCharacterList(data)
-    console.log(result) // returns Name | Element | Level | Friendship level | Constellation
+    if (!data) return console.log("Undefined error.")
+    const chara = await gi.simpleCharacterList(data)
+    const stats = await gi.simpleUserStats(data)
+    const world = await gi.simpleWorldExplorations(data)
+    console.log(chara + '\n\n' + stats + "\n\n" + world) // returns Name | Element | Level | Friendship level | Constellation
 }
 
 getCharacters(config.COOKIE_TOKEN, config.UID) // for more easier make a config.json, example on my repository
